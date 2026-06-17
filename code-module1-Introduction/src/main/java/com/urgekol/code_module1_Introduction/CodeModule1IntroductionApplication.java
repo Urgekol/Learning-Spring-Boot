fd74d9cda27806652571f0445bb55bb6b1f0c843
Arghadip Biswas <arghadipbiswas0@gmail.com>
2026-06-17 11:42:07 +0530

    Note-2

--- code-module1-Introduction/src/main/java/com/urgekol/code_module1_Introduction/CodeModule1IntroductionApplication.java
+++ code-module1-Introduction/src/main/java/com/urgekol/code_module1_Introduction/CodeModule1IntroductionApplication.java
@@ -6,10 +6,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeModule1IntroductionApplication
+public class CodeModule1IntroductionApplication implements CommandLineRunner
 {
+	@Autowired
+	Payment paymentObj;		// Spring creates: Payment paymentObj = new Payment();
+
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(CodeModule1IntroductionApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args)
+	{
+		paymentObj.paying();
+	}
 }
+
+/*
+	Why Not Call It Inside main()?
+	a. main() is static.
+	b. Dependency injection happens on objects.
+	c. paymentObj isn't available in the static context.
+*/
